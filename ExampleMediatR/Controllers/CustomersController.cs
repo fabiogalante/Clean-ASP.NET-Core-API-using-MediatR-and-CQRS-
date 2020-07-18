ca859b87bb10cdaab303b10b28f0d898d03469a9
Fabio Galante Mans <fabiogalantemans@gmail.com>
2020-07-18 20:09:21 -0300

    Add project files.

---
+++ ExampleMediatR/Controllers/CustomersController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using ExampleMediatR.Mapping;
+using ExampleMediatR.Queries;
+using ExampleMediatR.Repositories;
+using ExampleMediatR.Responses;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExampleMediatR.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CustomersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        
+        [HttpGet("")]
+        public async Task<IActionResult> GetCustomers()
+        {
+            var query = new GetAllCustomersQuery();
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpGet("{customerId}")]
+        public async Task<IActionResult> GetCustomer(Guid customerId)
+        {
+            var query = new GetCustomerByIdQuery(customerId);
+            var result = await _mediator.Send(query);
+            return result != null ? (IActionResult) Ok(result) : NotFound();
+        }
+
+        [HttpGet("{customerId}/orders")]
+        public async Task<IActionResult> GetCustomerOrders(Guid customerId)
+        {
+            var query = new GetCustomerOrdersQuery(customerId);
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+    }
+}
